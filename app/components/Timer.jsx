7fc2ba62b4d42b1ad93db8e8bc6f48b3bf9c0f82
Andres Caycho <acaycho@belatrix.com>
2017-02-01 17:33:32 -0500

    Adding Clock component and testing

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
 
+var Clock = require('Clock');
+
 var Timer = React.createClass({
 
   render: function() {
     return (
       <div>
         <p>Timer.jsx</p>
+        <Clock totalSeconds={845}/>
       </div>
     );
   }
